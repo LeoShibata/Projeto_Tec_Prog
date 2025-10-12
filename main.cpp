b29916480a57f17ea628c21c9172d2fbc72b8431
Arthur3040 <arthurnunedb1@gmail.com>
2025-10-12 15:58:21 -0300

    arrows movement test in keyboard

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 
 int main(int argc, char const *argv[])
 {
-    sf::RenderWindow window(sf::VideoMode(200,200), "Hello From SFML");
-    sf::CircleShape shape(100.f);
+    sf::RenderWindow window(sf::VideoMode(1200,1600), "Hello From SFML");
+    sf::CircleShape shape(101.f);
     shape.setFillColor(sf::Color::Magenta);
+    float vel_r = 3;
+    float vel_l = 3;
+    float vel_u = 3;
+    float vel_d = 3;
 
     while (window.isOpen())
     {
@@ -13,8 +17,35 @@ int main(int argc, char const *argv[])
         {
             if(event.type == sf::Event::Closed){
                 window.close();
+	    
             }
-        }
+	    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+    		// left key is pressed: move our character
+			printf("le keyboard\n");
+    			shape.move(-vel_l, 0.f);
+		}
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+                {
+                // left key is pressed: move our character
+                        printf("le keyboard\n");
+                        shape.move(vel_r, 0.f);
+                }
+	    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+                {
+                // left key is pressed: move our character
+                        printf("le keyboard\n");
+                        shape.move(0.f, -vel_u);
+                }
+	    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+                {
+                // left key is pressed: move our character
+                        printf("le keyboard\n");
+                        shape.move(0.f, vel_d);
+                }
+
+
+	}
         window.clear();
         window.draw(shape);
         window.display();
