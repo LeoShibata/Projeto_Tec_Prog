f43e87c73aa3ecf3bb73fd101c3e21ff7673eda2
MaquinAgricola <86669050+Arthur3040@users.noreply.github.com>
2025-11-12 18:17:21 -0300

    Add files via upload

---
+++ Element.hpp
@@ -0,0 +1,53 @@
+#ifndef ELEMENT_HPP
+#define ELEMENT_HPP 
+
+namespace List {
+    template <class TE>
+    class Element {
+        private:
+            Element<TE>* next;
+            TE* element;
+        
+        public:
+            Element();
+            ~Element();
+            
+            void setNext(Element<TE>* next);
+            void setElement(TE* element);
+            Element<TE>* getNext();
+            TE* getElement();
+    };
+
+    template <class TE>
+    Element<TE>::Element() :
+        next(nullptr),
+        element(nullptr)
+    {
+
+    }
+
+    template <class TE>
+    Element<TE>::~Element() { }
+
+    template <class TE>
+    void Element<TE>::setNext(Element<TE>* next) {
+        this->next = next;
+    }
+
+    template <class TE>
+    void Element<TE>::setElement(TE* element) {
+        this->element = element;
+    }
+
+    template <class TE>
+    Element<TE>* Element<TE>::getNext() {
+        return next;
+    }
+
+    template <class TE>
+    TE* Element<TE>::getElement() {
+        return element;
+    }
+}
+
+#endif
---
+++ EntityList.hpp
@@ -0,0 +1,27 @@
+#ifndef ENTITYLIST_HPP 
+#define ENTITYLIST_HPP
+
+#include <List/List.hpp>
+#include <Entities/Entity.hpp>
+
+namespace List {
+    class EntityList {
+        private:
+            List<Entities::Entity> objEntityList;
+        
+        public:
+            EntityList();
+            ~EntityList();
+            
+            void addEntity(Entities::Entity* entity);
+            void removeEntity(Entities::Entity* entity);
+            void removeEntity(int pos);
+            int getSize();
+            Entities::Entity* operator[](int pos);
+            
+            void executeAll();
+            void drawAll(sf::RenderWindow* window);
+    };
+}
+
+#endif
