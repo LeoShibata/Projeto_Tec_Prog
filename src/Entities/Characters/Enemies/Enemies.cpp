942c0563f93d454451f781b34a9fcc0fabb1526d
MaquinAgricola <86669050+Arthur3040@users.noreply.github.com>
2025-11-12 18:40:35 -0300

    Add files via upload

---
+++ src/Entities/Characters/Enemies/Enemies.cpp
@@ -0,0 +1,28 @@
+#include "Entities/Characters/Enemies/Enemies.hpp"
+#include "Entities/Characters/Player.hpp"
+
+namespace Entities::Characters {
+
+Player* Enemies::pPlayer = nullptr; 
+
+void Enemies::initialize() { }
+
+Enemies::Enemies(const sf::Vector2f position, const sf::Vector2f size, int maldade):    
+    Character(position, size, 10.f), 
+    nivel_maldade(maldade) 
+{
+    initialize();
+    typeId = IDs::enemy;
+}
+
+Enemies::~Enemies() { }
+
+void Enemies::setPlayer(Player* p) {
+    pPlayer = p;
+}
+
+void Enemies::playerCollide(Player *pPlayer) { }
+
+void Enemies::damaging(Player *pPlayer) { }
+
+}
---
+++ src/List/EntityList.cpp
@@ -0,0 +1,51 @@
+#include <List/EntityList.hpp>
+
+namespace List {
+
+EntityList::EntityList() :
+    objEntityList()
+{
+
+}
+
+EntityList::~EntityList() { }
+
+void EntityList::addEntity(Entities::Entity* entity) {
+    objEntityList.addElement(entity);
+}
+
+void EntityList::removeEntity(Entities::Entity* entity) {
+    objEntityList.removeElement(entity);
+}
+
+void EntityList::removeEntity(int pos) {
+    objEntityList.removeElement(pos);
+}
+
+int EntityList::getSize() {
+    return objEntityList.getSize();
+}
+
+Entities::Entity* EntityList::operator[](int pos) {
+    return objEntityList.operator[](pos);
+}
+
+void EntityList::executeAll() {
+    int size = objEntityList.getSize();
+    Entities::Entity* aux = nullptr;
+    for(int i = 0; i < size; i++) {
+        aux = objEntityList.operator[](i);
+        aux->execute();
+    }
+}
+
+void EntityList::drawAll(sf::RenderWindow* window) {
+    int size = objEntityList.getSize();
+    Entities::Entity* aux = nullptr;
+    for(int i = 0; i < size; i++) {
+        aux = objEntityList.operator[](i);
+        window->draw(aux->getBody());
+    }
+}
+
+}
